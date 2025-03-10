0111ecf36fcbf86e06dd3a31a74fa8d94454131d
Trevor Karuma <trevormukundi@gmail.com>
2025-03-10 23:13:12 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply)
 
+let random = Math.floor(Math.random()*100)+1;
+console.log(random)
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3% num4;
+console.log(mod)
+
+let numbers = [10,5,20,15];
+let max = Math.max(...numbers);
+console.log(max)
 // Write your code here
